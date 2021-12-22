041b42dbc52f775def535d0c5eb3b26000b0b3d2
romuant1 <romuant1@gmail.com>
2021-12-21 22:33:36 -0300

    enviando arquivos corrigidos

--- reducePaulinho.js
+++ reducePaulinho.js
@@ -1,10 +1,10 @@
 const numeros = [1,2,3,4, 6] 
 
 const somaDosNumeros = numeros.reduce((previ,curr)=> curr+previ)
-console.log('soma dos numeros é', somaDosNumeros)
+console.log('soma dos numeros é', +somaDosNumeros)
 
 const multDosNumeros = numeros.reduce((previ,curr)=>curr*previ)
-console.log('mult dos numeros é', multDosNumeros)
+console.log('mult dos numeros é', +multDosNumeros)
 
 multiplos2 = numeros.reduce((previ, item) => { 
     if (item % 2 === 0)  {
@@ -15,14 +15,28 @@ multiplos2 = numeros.reduce((previ, item) => {
     
 } ,[])
 console.log('multiplos de dois dos numeros é', multiplos2)
+const newValue = {}
+colecaoNumerosReducer = function (previ, curr, i) {
+    //
+    
+    previ[i] = curr**4
+    return previ
+}
 
-const colecaoNumeros = numeros.reduce((previ, item, i) => {
-   const operacaoComplicada = (parametro) => /* operação complicada */ parametro ** 4
-   previ[i] = operacaoComplicada(item)
-   return previ
-}, {})
+
+
+
+
+const colecaoNumeros = numeros.reduce(colecaoNumerosReducer, newValue)
+
+
+
+/* {}  */
+
 console.log('coleção dos numeros é', colecaoNumeros)
-console.log(colecaoNumeros[0])
+console.log('valor na posição 0: ', colecaoNumeros[0])
+
+
 
         
         
--- segundiMaiorNumero.js
+++ segundiMaiorNumero.js
@@ -1,16 +1,17 @@
 
 function getSecondLargest(nums) {
     
-        function comparaNumeros(a,b) { 
+    function comparaNumeros(a,b) { 
         if (a === b) return 0;
         if (a < b) return -1;
         if (a > b) return 1;
- }
-const numeros = [...new Set(nums)]
+    }
+    const numeros = [...new Set(nums)]
     numeros.sort(comparaNumeros);
 
-const last= numeros.pop();
-const secondlast= numeros.pop();
-return secondlast
+    numeros.pop();
+    const secondlast= numeros.pop();
+    return secondlast
 }
-console.log()
+console.log(getSecondLargest([2,4,6,6,68,56,78,-500,-12]))
+
