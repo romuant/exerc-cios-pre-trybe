71b3110620bbdc9cb7dde9bda4c776c272aede5f
romuant1 <romuant1@gmail.com>
2021-12-26 09:41:56 -0300

    atualiz

--- areaEPerimetro.js
+++ areaEPerimetro.js
@@ -1,4 +1,17 @@
-const PI = mach.PI;
+/*const PI = Mach.PI;
 const r= 2.6;
 console.log("area = " + PI*r*r);
-console.log("perimeter = " +PI*2*r);
+console.log("perimeter = " +PI*2*r);*/
+
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+  }
+  
+  
+  console.log(Math.PI);
+  // expected output: 3.141592653589793
+  
+  console.log(calculateCircumference(10));
+  // expected output: 62.83185307179586
+  
+  
