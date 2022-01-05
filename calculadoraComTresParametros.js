c0188b2c889189d0bcfba6947790d3581af96884
romuant1 <romuant1@gmail.com>
2022-01-05 20:05:02 -0300

    nova atualização para calculadora com tres parametros

--- calculadoraComTresParametros.js
+++ calculadoraComTresParametros.js
@@ -1,12 +1,25 @@
 //função queimplementa tres argumantos sendo adição, subtração e a operação "+" ou "-" como terceiro argumento;
-function adicaoSubtracao(numero,outroNumero,operacao)  {
+function calculadora(numero,outroNumero,operacao)  {
     if (operacao ==="+")  {
         return numero + outroNumero
 
     }else if (operacao==="-"){
             return numero - outroNumero
+        }else if(operacao ==="*")  {
+            return numero * outroNumero
+        }else if(operacao ==="/")  {
+            return numero / outroNumero
+        }else if(operacao ==="**")  {
+            return numero ** outroNumero
         }else{
+            return 0
+        }
     }
-        return 0
+        
     
-}
+
+console.log("resultado da operação de adição de 3 + 4 é  :"+calculadora(3,4,"+"))
+console.log("resultado da operação de subtração de 3 - 4 é :     "+calculadora(3,4,"-"))
+console.log("resultado da operação de multiplicação entre 3 e 4 é:  "+calculadora(3,4,"*"))
+console.log("resultado da operação de divisão de 3 por 4 é:  "+calculadora(3,4,"/"))
+console.log("resultado da operação de exponenciação de 4 na base 3 é:  "+calculadora(3,4,"**"))
