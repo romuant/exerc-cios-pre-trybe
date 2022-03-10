e1b01be3cc93c407eaabfec7e70600bd16b1a8df
romuant <romuant1@gmail.com>
2022-03-10 09:37:14 -0300

    melhorando codigo

--- areaEPerimetro.js
+++ areaEPerimetro.js
@@ -11,7 +11,7 @@ function calculateCircumference(radius) {
   console.log(Math.PI);
   // expected output: 3.141592653589793
   
-  console.log(calculateCircumference(10));
+  console.log("Circunferencia = ",calculateCircumference(10));
   // expected output: 62.83185307179586
   
   
